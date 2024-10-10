b1a220a7c7c1c00d10a86516262c840a496049c0
EminliAysun05 <aeminli232@gmail.com>
2024-10-10 21:31:18 +0400

    task eneded

---
+++ Podcast.BLL/ViewModels/EpisodeViewModels/EpisodeRelatedViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podcast.BLL.ViewModels.EpisodeViewModels
+{
+    public class EpisodeRelatedViewModel
+    {
+        public EpisodeViewModel? SelectedEpisode { get; set; }
+        public List<EpisodeViewModel>? RelatedEpisodes { get; set; }
+    }
+}
